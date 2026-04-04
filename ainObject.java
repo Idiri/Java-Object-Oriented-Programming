ef74c2de24c5e75e1b32b8cf9069af537b5d07a9
Idiri <idirismuhu@gmail.com>
2026-04-04 02:21:09 +0200

    cleanup: fix typo in MainObject filename

--- ainObject.java
+++
@@ -1,7 +0,0 @@
-public class mainObject{
-    public static void main(String[] args){
-
-        Car2 car = new Car2();
-        car.go();
-    }
-}
--- mainObject.java
+++ mainObject.java
@@ -2,8 +2,6 @@ public class mainObject{
     public static void main(String[] args){
 
         Car2 car = new Car2();
-        car.start();
-        car.stop();
-        car.brake();
+        car.go();
     }
 }
